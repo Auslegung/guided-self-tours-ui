81a11fabce0ecd8a564532a5baa4ddea666b4e92
Jason Fry <jmphry@gmail.com>
2016-11-30 19:12:11 -0500

    Still broken for no discernable reason

--- public/js/router.js
+++ public/js/router.js
@@ -1,27 +1,42 @@
 (function() {
   angular.module('tour', ['ui-router'])
-    .config(MainRouter);
+  .config(MainRouter);
 
-    MainRouter.$inject = ['$stateProvider', '$urlRouterProvider']
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider']
 
-    function MainRouter($stateProvider, $urlRouterProvider) {
-      $stateProvider
-        .state('home', {
-          url: '/',
-          templateUrl: '../partials/_home.html'
-        })
-        .state('signup', {
-          url: '/signup',
-          templateUrl: '../partials/_signup.html'
-        })
-        .state('login', {
-          url: '/login',
-          templateUrl: '../partials/_login.html'
-        })
-        .state('user', {
-          url: '/user',
-          templateUrl: '../partials/_user.html'
-        })
-        $urlRouterProvider.otherwise('/');
-      }
+  function MainRouter($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '../partials/_home.html'
+    })
+  }
 })();
+
+// (function() {
+//   angular.module('tour', ['ui-router'])
+//     .config(MainRouter);
+//
+//     MainRouter.$inject = ['$stateProvider', '$urlRouterProvider']
+//
+//     function MainRouter($stateProvider, $urlRouterProvider) {
+//       $stateProvider
+//         .state('home', {
+//           url: '/',
+//           templateUrl: '../partials/_home.html'
+//         })
+//         .state('signup', {
+//           url: '/signup',
+//           templateUrl: '../partials/_signup.html'
+//         })
+//         .state('login', {
+//           url: '/login',
+//           templateUrl: '../partials/_login.html'
+//         })
+//         .state('user', {
+//           url: '/user',
+//           templateUrl: '../partials/_user.html'
+//         })
+//         $urlRouterProvider.otherwise('/');
+//       }
+// })();
